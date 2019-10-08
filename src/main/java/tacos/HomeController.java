1fc2e017a4c2d5c2218323871cf25117915dfcc8
Erik Moody <erikmmoody@gmail.coms>
2019-10-07 22:22:26 -0500

    Initial commit

---
+++ src/main/java/tacos/HomeController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author erikmoody
+ */
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+    
+}
